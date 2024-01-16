495a4c0088ee7e1e49d0f7cb9d2bae80c66c6de0
gabe <gabrieltucker260@gmail.com>
2024-01-16 13:52:41 -0800

    finsished game

--- Sphinx/Models/SphinxRiddles.cs
+++ Sphinx/Models/SphinxRiddles.cs
@@ -1,52 +1,34 @@
 using System;
-using system.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Sphinx.Models
 {
     public class SphinxRiddles
     {
+
+        public int AmountOfRiddles
+        { get {return riddles.Count;}}
         private List<String> riddles = new List<string>
         {
           "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?",
           "The more you take, the more you leave behind. What am I?",
           "I have keys but no locks. I have space but no room. You can enter, but you can't go inside. What am I?"
-        }
+        };
         private List<String> riddlesAnswers = new List<string>
         {
           "echo",
           "footsteps",
           "keyboard"
-        }
+        };
 
       public string GetRiddle(int index)
         {
             return riddles[index];
         }
 
-        public string GetAnswer(int index)
+          public string GetAnswer(int index)
         {
             return riddlesAnswers[index];
         }
-    }
-}
-
-
-public string CheckType()
-{
-    if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2)))
-    {
-        return "not a triangle";
-    }
-    else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3)))
-    {
-        return "scalene triangle";
-    }
-    else if ((Side1 == Side2) && (Side1 == _side3))
-    {
-        return "equilateral triangle";
-    }
-    else
-    {
-        return "isosceles triangle";
     }
 }
--- Sphinx/Program.cs
+++ Sphinx/Program.cs
@@ -1,7 +1,114 @@
-class Program
+using System;
+using Sphinx.Models;
+using System.Threading;
+
+// using ConsoleColors;
+
+namespace Sphinx
 {
-  static void Main()
+  class Program
   {
-    // We'll add user interface code here.
+    static void Main()
+    {
+
+      Console.WriteLine("--#--#--#--#--#--#--#--#--#--#--");
+      Console.WriteLine("Welcome to Sphinx Riddles");
+      Console.ForegroundColor = ConsoleColor.DarkYellow;
+      Console.WriteLine(@"                              .sSSSSSSSs
+                              sSS=''^^^'s
+                  /\       , /  \_\_\|_/_)
+                 /';;     /| \\\/.-. .-./
+                / \;|    /. \,S'  -   - |
+               / -.;|    | '.SS     _|  ;
+              ; '-.;\,   |'-.SS\   __  /S
+              | _  ';\\.  \' SSS\_____/SS
+              |  '- ';\\.  \_SSS[_____]SS
+              \ '--.-';;-. __SSS/\    SSS
+               \  .--' ';;'.=SSS`\\_\_SSS
+                `._ .-'` _';;..=.=.=.\.=\
+                   ;-._-'  _.;\.=.=.=.|.=|
+         ,     _.-'    `'=._  ;\=.=__/__/
+         )\ .'`   __        '.;|.=.=.=./
+         (_\   .-`  '.   |    \/=.=.=/`
+          /\\         \-,|     |.--'|
+         /  \`,       //  \    | |  |
+        ( (__) )  _.-'--,  \   | |  '--,
+         ;----' -'--,__}}}  \  '--, __}}}
+         \_________}}}       \___}}}");
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.WriteLine("The sphinx will ask you a question and you must answer if you value your life");
+      Console.WriteLine("Type 'begin' to start, or enter anything else to flee");
+      Console.WriteLine("--#--#--#--#--#--#--#--#--#--#--");
+      string userResponse = Console.ReadLine();
+      if (userResponse == "begin" || userResponse == "Begin" || userResponse == "BEGIN")
+      {
+        StartRiddle();
+      }
+      else
+      {
+        Console.WriteLine("Farewell!");
+      }
+    }
+
+    static void StartRiddle()
+    {
+      SphinxRiddles sphinxRiddles = new SphinxRiddles();
+
+      Random rnd = new Random();
+      int index = rnd.Next(sphinxRiddles.AmountOfRiddles);
+
+      string newRiddle = sphinxRiddles.GetRiddle(index);
+      string newAnswer = sphinxRiddles.GetAnswer(index);
+
+      Console.WriteLine(newRiddle);
+
+      string userResponse = Console.ReadLine();
+
+      if (userResponse == newAnswer)
+      {
+        Console.WriteLine("You win!");
+        ColorfulAnimation("You win!");
+      }
+      else
+      {
+        Console.WriteLine("You Lose!");
+        ColorfulAnimation("You lose!");
+      }
+    }
+
+    static void ColorfulAnimation(string statusMessage)
+    {
+        Thread.Sleep(2000);
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 30; j++)
+            {
+                Console.Clear();
+
+                // steam
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("       . . . . o o o o o o");
+                for (int s = 0; s < j / 2; s++)
+                {
+                    Console.Write(" o", ConsoleColor.White);
+                }
+                Console.WriteLine();
+
+                var margin = "".PadLeft(j);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(margin + "                _____      o", ConsoleColor.Magenta);
+                Console.WriteLine(margin + "       ____====  ]OO|_n_n__][.", ConsoleColor.Magenta);
+                Console.WriteLine(margin + "      [________]_|__|________)< ", ConsoleColor.Magenta);
+                Console.WriteLine(margin + "       oo    oo  'oo OOOO-| oo\\_", ConsoleColor.Magenta);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("   +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+", ConsoleColor.Magenta);
+                Console.WriteLine("");
+                Console.WriteLine(statusMessage);
+                Console.WriteLine("Press CTRL+C to Exit");
+
+                Thread.Sleep(200);
+            }
+        }
+    }
   }
 }
